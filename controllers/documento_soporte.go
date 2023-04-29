446bcf0e7bf62f0ebdbc86669c05aef0d47d9611
lud <ludwinggsanchez@gmail.com>
2023-04-28 19:41:11 -0500

    feat udistrital/autenticacion_mid#60: Conexión SIDEAP
    - Se agregan controladores para consultar la información básica, hoja de vida y documento.

---
+++ controllers/documento_soporte.go
@@ -0,0 +1,26 @@
+package controllers
+
+import (
+	beego "github.com/beego/beego/v2/server/web"
+)
+
+// DocumentoSoporteController operations for Documento_soporte
+type DocumentoSoporteController struct {
+	beego.Controller
+}
+
+// URLMapping ...
+func (c *DocumentoSoporteController) URLMapping() {
+	c.Mapping("GetOne", c.GetOne)
+}
+
+// GetOne ...
+// @Title GetOne
+// @Description get Documento_soporte by id
+// @Param	id		path 	string	true		"The key for staticblock"
+// @Success 200 {object} models.Documento_soporte
+// @Failure 403 :id is empty
+// @router /:id [get]
+func (c *DocumentoSoporteController) GetOne() {
+
+}
