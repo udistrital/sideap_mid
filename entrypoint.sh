18ddf3198ca94c95ebdb7a0274006acf73456fce
lud <ludwinggsanchez@gmail.com>
2023-05-02 10:33:09 -0500

    feat udistrital/autenticacion_mid#60: SIDEAP
    - Se cambia consulta de credenciales para acceder a servicios del sideap.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -o pipefail
 
 if [ -n "${PARAMETER_STORE:-}" ]; then
   export SIDEAP_USER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/sideap_mid/sideap/username --output text --query Parameter.Value)"
-  export SIDEAP_KEY="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/sideap_mid/sideap/password --output text --query Parameter.Value)"
+  export SIDEAP_KEY="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/sideap_mid/sideap/key --output text --query Parameter.Value)"
 fi
 
 python api.py
